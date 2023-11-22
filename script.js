fa584c1a1b1a006c87c0376cf40168cf47b8b9c1
Alvin Angelo <98679731+uwdwd@users.noreply.github.com>
2023-11-22 21:00:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,21 +8,3 @@ window.onscroll = function () {
     header.classList.remove("navbarDark");
   }
 };
-
-function openFile(filePath) {
-  window.open(
-    "C:\Users\Alvin Angelo\Desktop\DTW 1\Portofolio.github.io\banner is night 3x1 m.pdf",
-    "_blank"
-  );
-}
-
-function openFile(filePath) {
-  window.open("C:\Users\Alvin Angelo\Desktop\DTW 1\Portofolio.github.io\Sertifikat Falcon 12.pdf");
-}
-
-function openFile(filePath) {
-  window.open(
-    "C:UsersAlvin AngeloDesktopDTW 1Portofolio.github.ioGathering Information Systems 2023 Certificate of Appreciation.pdf",
-    "_blank"
-  );
-}
